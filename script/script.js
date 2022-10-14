51cb580cd6a99325c11584bebc2db93ccf12c685
heesung2lee <110263644+heesung2lee@users.noreply.github.com>
2022-10-13 23:01:31 -0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,7 +7,6 @@ for (var i = 0; i < words.length; i++) {
   splitLetters(words[i]);
 }
 
-
 function changeWord() {
   var cw = wordArray[currentWord];
   var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
